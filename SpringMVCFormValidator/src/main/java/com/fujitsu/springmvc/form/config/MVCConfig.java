833e27d2ef381878174cb66ea0ad7b41859e630f
imsayyad <85538931+imsayyad@users.noreply.github.com>
2023-02-16 08:10:48 +0530

    Uploaded via Company laptop
    First commit

---
+++ SpringMVCFormValidator/src/main/java/com/fujitsu/springmvc/form/config/MVCConfig.java
@@ -0,0 +1,23 @@
+package com.fujitsu.springmvc.form.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+import org.springframework.web.servlet.view.JstlView;
+
+@Controller
+@EnableWebMvc
+@ComponentScan (basePackages = "com.fujitsu.springmvc.form")
+public class MVCConfig implements WebMvcConfigurer {
+	@Bean
+	public InternalResourceViewResolver resolver() {
+		InternalResourceViewResolver resolver = new InternalResourceViewResolver();
+		resolver.setViewClass(JstlView.class);
+		resolver.setPrefix("/WEB-INF/views/");
+		resolver.setSuffix(".jsp");
+		return resolver;
+	}
+}
